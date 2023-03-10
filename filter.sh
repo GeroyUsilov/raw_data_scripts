80ac726585d6d979c44cdd8326023eae4ee2ff84
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-03-09 21:30:13 -0600

    fixed variable assignment issues

--- filter.sh
+++ filter.sh
@@ -2,14 +2,14 @@
 export DATADIR=/project2/amurugan/danio_rnaseq
 sample=$1
 echo "$sample filtering"
-r1_in_gz= $DATADIR/$sample/*R1*.gz 
-r2_in_gz= $DATADIR/$sample/*R2*.gz 
+r1_in_gz=$DATADIR/$sample/*R1*.gz 
+r2_in_gz=$DATADIR/$sample/*R2*.gz 
 echo "R1 filename zipped $r1_in_gz"
 echo "R2 filename zipped $r1_in_gz"
 gunzip $r1_in_gz
 gunzip $r2_in_gz
-r1_in= $DATADIR/$sample/*R1*.fastq
-r2_in= $DATADIR/$sample/*R2*.fastq
+r1_in=$DATADIR/$sample/*R1*.fastq
+r2_in=$DATADIR/$sample/*R2*.fastq
 python filtering.py $r1_in $r2_in 30 35
 files_to_zip=( $DATADIR/$sample/*.fastq )
 gzip ${files_to_zip[*]}
