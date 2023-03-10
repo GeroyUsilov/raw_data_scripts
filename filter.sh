c541b5d47a92d5d8361ade58c2ff0bbcacc06d1f
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-03-09 21:50:00 -0600

    debugging filter script

--- filter.sh
+++ filter.sh
@@ -8,9 +8,11 @@ echo "R1 filename zipped $r1_in_gz"
 echo "R2 filename zipped $r1_in_gz"
 gunzip $r1_in_gz
 gunzip $r2_in_gz
-r1_in=$DATADIR/$sample/*R1*.fastq
-r2_in=$DATADIR/$sample/*R2*.fastq
-python filtering.py $r1_in $r2_in 30 35
+r1_in=($DATADIR/$sample/*R1*.fastq)
+r2_in=($DATADIR/$sample/*R2*.fastq)
+echo "${r1_in[0]}"
+echo "${r2_in[0]}"
+python filtering.py "${r1_in[0]}" "${r2_in[0]}" 30 35
 files_to_zip=( $DATADIR/$sample/*.fastq )
 gzip ${files_to_zip[*]}
 echo "$sample is done"
