8cc6169aaa137cd7345daad32f4baffc5fee90e2
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-03-31 01:16:51 -0500

    moving output files

--- rna_map_loop_filter.sh
+++ rna_map_loop_filter.sh
@@ -7,3 +7,5 @@ STAR --runThreadN 15 --outReadsUnmapped Fastx --outFileNamePrefix $DATADIR/$samp
 echo "$sample is done"
 samtools view -bo $DATADIR/$sample/${sample}Aligned.out.bam $DATADIR/$sample/${sample}Aligned.out.sam
 rm $DATADIR/$sample/${sample}Aligned.out.sam
+mv rna_map_sbatch.err $DATADIR/$sample
+mv rna_map_sbatch.out $DATADIR/$sample
--- sickle.sh
+++ sickle.sh
@@ -4,3 +4,5 @@ sample=$1
 export INPUT_R1=$DATADIR/$sample/EJ*R1*fastq*.gz
 export INPUT_R2=$DATADIR/$sample/EJ*R2*fastq*.gz
 sickle pe -f $INPUT_R1 -r $INPUT_R2 -o "${DATADIR}/${sample}/${sample}_r1_filtered.fastq.gz" -p "${DATADIR}/${sample}/${sample}_r2_filtered.fastq.gz" -g -t sanger -q 35 -l 30 -s $DATADIR/$sample/trimmed_singles.fastq
+mv sickle_sbatch.out $DATADIR/$sample
+mv sickle_sbatch.err $DATADIR/$sample
