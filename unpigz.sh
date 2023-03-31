8e064fe6ea692e4420583cd3e7baeb4a8e938df5
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-03-30 21:06:32 -0500

    fixed typo

--- unpigz.sh
+++ unpigz.sh
@@ -4,5 +4,5 @@ export DATADIR=/project2/amurugan/danio_rnaseq/$sample
 export INPUT_R1_zipped=$DATADIR/EJ*R1*fastq*.gz
 export INPUT_R2_zipped=$DATADIR/EJ*R2*fastq*.gz
 
-unpigz INPUT_R1_zipped
-unpigz INPUT_R2_zipped
+unpigz $INPUT_R1_zipped
+unpigz $INPUT_R2_zipped
