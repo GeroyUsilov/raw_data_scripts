2ff55635df854ff891076299415e3b4290cde49d
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-03-30 22:01:34 -0500

    sickle revised

--- sickle.sh
+++ sickle.sh
@@ -2,4 +2,6 @@
 export DATADIR=/project2/amurugan/danio_rnaseq
 sample=$1
 files=( $DATADIR/$sample/*.gz )
-sickle pe -f "${files[0]}" -r "${files[1]}" -o "${DATADIR}/${sample}/${sample}_r1_filtered.fastq.gz" -p "${DATADIR}/${sample}/${sample}_r2_filtered.fastq.gz" -g -t sanger -q 35 -l 100 -s $DATADIR/$sample/trimmed_singles.fastq
+export INPUT_R1=$DATADIR/EJ*R1*fastq*.gz
+export INPUT_R2=$DATADIR/EJ*R2*fastq*.gz
+sickle pe -f $INPUT_R1 -r $INPUT_R2 -o "${DATADIR}/${sample}/${sample}_r1_filtered.fastq.gz" -p "${DATADIR}/${sample}/${sample}_r2_filtered.fastq.gz" -g -t sanger -q 35 -l 100 -s $DATADIR/$sample/trimmed_singles.fastq
