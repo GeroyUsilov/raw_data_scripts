388dc03a163d663523ff01e6058366472f2246dd
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-03-31 04:01:36 -0500

    pigz

--- pigz.sh
+++ pigz.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-sample=$1
-export DATADIR=/project2/amurugan/danio_rnaseq/$sample
-export INPUT_R1=$DATADIR/EJ*R1*fastq*
-export INPUT_R2=$DATADIR/EJ*R2*fastq*
-
-pigz $INPUT_R1
-
-pigz $INPUT_R2
+pigz $1
