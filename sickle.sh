a1bc461ca74a7e2bd611d1c2d77309241bc88bd1
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-03-30 22:07:53 -0500

    sickle params

--- sickle.sh
+++ sickle.sh
@@ -4,4 +4,4 @@ sample=$1
 files=( $DATADIR/$sample/*.gz )
 export INPUT_R1=$DATADIR/EJ*R1*fastq*.gz
 export INPUT_R2=$DATADIR/EJ*R2*fastq*.gz
-sickle pe -f $INPUT_R1 -r $INPUT_R2 -o "${DATADIR}/${sample}/${sample}_r1_filtered.fastq.gz" -p "${DATADIR}/${sample}/${sample}_r2_filtered.fastq.gz" -g -t sanger -q 35 -l 100 -s $DATADIR/$sample/trimmed_singles.fastq
+sickle pe -f $INPUT_R1 -r $INPUT_R2 -o "${DATADIR}/${sample}/${sample}_r1_filtered.fastq.gz" -p "${DATADIR}/${sample}/${sample}_r2_filtered.fastq.gz" -g -t sanger -q 36 -l 40 -s $DATADIR/$sample/trimmed_singles.fastq
