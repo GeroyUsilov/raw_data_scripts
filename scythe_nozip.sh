8bb1bf320749fcbe530e14a95fac4f36e7258f47
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-03-30 20:32:07 -0500

    changed regex

--- scythe_nozip.sh
+++ scythe_nozip.sh
@@ -2,8 +2,8 @@
 sample=$1
 export DATADIR=/project2/amurugan/danio_rnaseq/$sample
 export ADAPTER=$HOME/raw_data_scripts/nexterra.fa
-export INPUT_R1_zipped=$DATADIR/EJ*R1*fastq.gz
-export INPUT_R2_zipped=$DATADIR/EJ*R2*fastq.gz
+export INPUT_R1_zipped=$DATADIR/EJ*R1*fastq*.gz
+export INPUT_R2_zipped=$DATADIR/EJ*R2*fastq*.gz
 echo $1
 
 export OUTPUT_R1=$DATADIR/${sample}_r1_adapter_trimmed.fastq
