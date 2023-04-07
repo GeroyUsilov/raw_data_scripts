fb666337ab3a6b677ddec9d658e141928077f88c
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-04-07 07:49:17 -0500

    added test files

--- rna_map_loop_filter.sh
+++ rna_map_loop_filter.sh
@@ -2,7 +2,7 @@
 export DATADIR=/project2/amurugan/danio_rnaseq
 export GENOMEDIR=/project2/amurugan/danio_genomes/STAR_GRCz11_12102020
 sample=$1
-files=( $DATADIR/$sample/*_filtered.fastq.gz )
+files=( $DATADIR/$sample/*.fastq.gz )
 STAR --runThreadN 15 --outReadsUnmapped Fastx --outFileNamePrefix $DATADIR/$sample/$sample  --readFilesCommand zcat --genomeDir $GENOMEDIR --quantMode GeneCounts --readFilesIn "${files[0]}" "${files[1]}" 
 echo "$sample is done"
 samtools view -bo $DATADIR/$sample/${sample}Aligned.out.bam $DATADIR/$sample/${sample}Aligned.out.sam
