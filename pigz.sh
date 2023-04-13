6c876a942ffacd44f7b927302d3fee85bf212bd2
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-04-13 15:08:03 -0500

    fix pigz

--- pigz.sh
+++ pigz.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-pigz $1
+sample=$1
+export DATADIR=/project2/amurugan/danio_rnaseq/$sample
+export OUTDIR=/scratch/midway2/cjrusso/danio_nosedive_filtered/$sample
+zipped_files=$DATADIR/EJ*.gz
+cp ${zipped_files[*]} $OUTDIR
+zipped_files=$OUTDIR/EJ*.gz
+unpigz ${zipped_files[*]}
