3cd2b95fb783c099d90d0669747752482690ea54
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-04-13 17:17:37 -0500

    change filter

--- rna_map_loop_filter.sh
+++ rna_map_loop_filter.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-export DATADIR=/project2/amurugan/danio_rnaseq
+export DATADIR=/scratch/midway2/cjrusso/danio_nosedive_filtered
 export GENOMEDIR=/project2/amurugan/danio_genomes/STAR_GRCz11_12102020
 sample=$1
-files=( $DATADIR/$sample/*.fastq.gz )
-STAR --runThreadN 15 --outReadsUnmapped Fastx --outFileNamePrefix $DATADIR/$sample/$sample  --readFilesCommand zcat --genomeDir $GENOMEDIR --quantMode GeneCounts --readFilesIn "${files[0]}" "${files[1]}" 
+files=( $DATADIR/$sample/*_filtered.fastq )
+STAR --runThreadN 15 --outReadsUnmapped Fastx --outFileNamePrefix $DATADIR/$sample/$sample  --genomeDir $GENOMEDIR --quantMode GeneCounts --readFilesIn "${files[0]}" "${files[1]}" 
 echo "$sample is done"
-samtools view -bo $DATADIR/$sample/${sample}Aligned.out.bam $DATADIR/$sample/${sample}Aligned.out.sam
-rm $DATADIR/$sample/${sample}Aligned.out.sam
