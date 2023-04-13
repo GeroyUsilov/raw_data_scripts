38779245537683fc96467dbcc9b575d9c6fa9c62
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-04-13 16:18:34 -0500

    fixed filter script

--- filter.sh
+++ filter.sh
@@ -5,8 +5,8 @@ echo "$sample filtering"
 r1_in=$OUTDIR/*R1*.fastq 
 r2_in=$OUTDIR/*R2*.fastq
 
-echo "${r1_in[0]}"
-echo "${r2_in[0]}"
-python efficient_filtering.py "${r1_in[0]}" "${r2_in[0]}" 28 35 $OUTDIR
+echo ${r1_in[0]}
+echo ${r2_in[0]}
+python efficient_filtering.py ${r1_in[0]} ${r2_in[0]} 28 35 $OUTDIR
 
 echo "$sample is done"
