0a88ce9f7cc1d82d35f0c99e5b8c0cb8f60095a7
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-04-13 15:07:09 -0500

    pigz modified

--- efficient_filtering.py
+++ efficient_filtering.py
@@ -4,10 +4,11 @@ import sys
 from filtering_fn import *
 
 print(sys.argv)
+output_dir = sys.argv[5]
 r1_filename = sys.argv[1]
 r2_filename = sys.argv[2]
-r1_output_filename = r1_filename[:-6] + '_filtered' + '.fastq' 
-r2_output_filename = r2_filename[:-6] + '_filtered' + '.fastq' 
+r1_output_filename = output_dir + "r1" + '_nosedive_filtered' + '.fastq' 
+r2_output_filename = output_dir +  "r2" + '_nosedive_filtered' + '.fastq' 
 seq_ln = 100
 n_seqs = count_seqs(r1_filename)
 print(r1_filename)
@@ -17,16 +18,16 @@ start_filter_pos = int(sys.argv[4])
 input_seq_iterator_r1 = SeqIO.parse(r1_filename, "fastq")
 input_seq_iterator_r2 = SeqIO.parse(r2_filename, "fastq")
 
-input_seq_iterator_r1 = (records[0] for records in zip(input_seq_iterator_r1,input_seq_iterator_r2) if (np.mean(records[0].letter_annotations["phred_quality"][start_filter_pos:seq_ln]) > min_avg_score) and (np.mean(records[1].letter_annotations["phred_quality"][start_filter_pos:seq_ln]) > min_avg_score))
+input_seq_iterator_r1_filtered = (records[0] for records in zip(input_seq_iterator_r1,input_seq_iterator_r2) if (np.mean(records[0].letter_annotations["phred_quality"][start_filter_pos:seq_ln]) > min_avg_score) and (np.mean(records[1].letter_annotations["phred_quality"][start_filter_pos:seq_ln]) > min_avg_score))
 
 with open(r1_output_filename, "w") as output_handle:
-    SeqIO.write(input_seq_iterator_r1, output_handle, "fastq")
+    SeqIO.write(input_seq_iterator_r1_filtered, output_handle, "fastq")
 
 input_seq_iterator_r1 = SeqIO.parse(r1_filename, "fastq")
 input_seq_iterator_r2 = SeqIO.parse(r2_filename, "fastq")
 
-input_seq_iterator_r2 = (records[1] for records in zip(input_seq_iterator_r1,input_seq_iterator_r2) if (np.mean(records[0].letter_annotations["phred_quality"][start_filter_pos:seq_ln]) > min_avg_score) and (np.mean(records[1].letter_annotations["phred_quality"][start_filter_pos:seq_ln]) > min_avg_score))
+input_seq_iterator_r2_filtered = (records[1] for records in zip(input_seq_iterator_r1,input_seq_iterator_r2) if (np.mean(records[0].letter_annotations["phred_quality"][start_filter_pos:seq_ln]) > min_avg_score) and (np.mean(records[1].letter_annotations["phred_quality"][start_filter_pos:seq_ln]) > min_avg_score))
 
 
 with open(r2_output_filename, "w") as output_handle:
-    SeqIO.write(input_seq_iterator_r2, output_handle, "fastq")
+    SeqIO.write(input_seq_iterator_r2_filtered, output_handle, "fastq")
--- filter.sh
+++ filter.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-export DATADIR=/project2/amurugan/danio_rnaseq
 sample=$1
+export DATADIR=/project2/amurugan/danio_rnaseq
+export OUTDIR=/scratch/midway2/cjrusso/danio_nosedive_filtered/$sample/
 echo "$sample filtering"
 r1_in_gz=$DATADIR/$sample/*R1*.gz 
 r2_in_gz=$DATADIR/$sample/*R2*.gz 
 echo "R1 filename zipped $r1_in_gz"
-echo "R2 filename zipped $r1_in_gz"
-gunzip $r1_in_gz
-gunzip $r2_in_gz
+echo "R2 filename zipped $r2_in_gz"
+unpigz $r1_in_gz
+unpigz $r2_in_gz
 r1_in=($DATADIR/$sample/*R1*.fastq)
 r2_in=($DATADIR/$sample/*R2*.fastq)
 echo "${r1_in[0]}"
 echo "${r2_in[0]}"
-python efficient_filtering.py "${r1_in[0]}" "${r2_in[0]}" 30 35
+python efficient_filtering.py "${r1_in[0]}" "${r2_in[0]}" 28 35 $OUTDIR
+echo "zipping"
 files_to_zip=( $DATADIR/$sample/*.fastq )
-gzip ${files_to_zip[*]}
+pigz ${files_to_zip[*]}
+files_to_zip=( $OUTDIR*.fastq )
+pigz ${files_to_zip[*]}
 echo "$sample is done"
