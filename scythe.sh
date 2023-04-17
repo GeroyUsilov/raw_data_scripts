a08f6d6e62f2c7fc930e996a5034517eb71374e1
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-04-17 17:42:14 -0500

    changed scythe params

--- scythe.sh
+++ scythe.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 sample=$1
-export DATADIR=/project2/amurugan/danio_rnaseq/$sample
+export DATADIR=/scratch/midway2/cjrusso/danio_nosedive_filtered
 export ADAPTER=$HOME/raw_data_scripts/nexterra.fa
+files=( $DATADIR/$sample/*_filtered.fastq )
 echo $1
-export INPUT_R1=$DATADIR/EJ*R1*fastq*
+export INPUT_R1="${files[0]}"
 export OUTPUT_R1=$DATADIR/${sample}_r1_adapter_trimmed.fastq
 $HOME/scythe/scythe -a $ADAPTER -o $OUTPUT_R1 $INPUT_R1
 echo "R1 done"
 
-export INPUT_R2=$DATADIR/EJ*R2*fastq*
+export INPUT_R2="${files[1]}"
 export OUTPUT_R2=$DATADIR/${sample}_r2_adapter_trimmed.fastq
 $HOME/scythe/scythe -a $ADAPTER -o $OUTPUT_R2 $INPUT_R2
 
