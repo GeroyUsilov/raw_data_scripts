052dac918641798f956c0c52c2558f2a249f104f
GeroyUsilov <christopherjoel.russo@gmail.com>
2023-04-17 19:12:48 -0500

    add adapter trim file name

--- rna_map_loop_filter.sh
+++ rna_map_loop_filter.sh
@@ -2,6 +2,6 @@
 export DATADIR=/scratch/midway2/cjrusso/danio_nosedive_filtered
 export GENOMEDIR=/project2/amurugan/danio_genomes/STAR_GRCz11_12102020
 sample=$1
-files=( $DATADIR/$sample/*_filtered.fastq )
+files=( $DATADIR/$sample/*_adapter_trimmed.fastq )
 STAR --runThreadN 15 --outReadsUnmapped Fastx --outFileNamePrefix $DATADIR/$sample/$sample  --genomeDir $GENOMEDIR --quantMode GeneCounts --readFilesIn "${files[0]}" "${files[1]}" 
 echo "$sample is done"
